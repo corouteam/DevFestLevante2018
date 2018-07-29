1a646b281fce3d18bb7e52da38012e5d9390c852
Paolo Rotolo <paolorotolo@ubuntu.com>
2018-07-29 20:24:12 +0200

    Rebuild SchedulePage

--- lib/SchedulePage.dart
+++ lib/SchedulePage.dart
@@ -2,6 +2,7 @@ import 'package:devfest_levante/DevFestActivity.dart';
 import 'package:devfest_levante/TalkPage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class SchedulePage extends StatelessWidget {
   @override
@@ -41,11 +42,14 @@ class SchedulePage extends StatelessWidget {
             }
           }
 
-          return new ListView.builder(
-            itemCount: snapshot.data.documents.length,
-            padding: const EdgeInsets.only(top: 10.0),
-            itemBuilder: (context, index) =>
-                _buildListItem(context, activities[index]),
+          return Container(
+            decoration: BoxDecoration(color: Colors.white),
+            child: new ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              padding: const EdgeInsets.only(top: 10.0),
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, activities[index]),
+            ),
           );
         });
   }
@@ -56,24 +60,57 @@ class SchedulePage extends StatelessWidget {
 }
 
 class ActivityTile extends StatelessWidget {
-  final DevFestActivity activity;
-
   const ActivityTile(this.activity);
+
+  final DevFestActivity activity;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: new GestureDetector(
-        // Build talk object
-        onTap: () => _openTalkPage(context, activity),
-        child: ,
 
-      ),
-    );
+        // Show TIME at start
+        leading: Column(
+          // Allign all at start
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // Will have 2 child (start time, end time)
+          children: <Widget>[
+            Text(
+              formatTime(activity.start),
+              style: TextStyle(color: Colors.indigoAccent),
+              textScaleFactor: 1.5,
+            ),
+            Text(formatTime(activity.end))
+          ],
+        ),
+
+        // Show activity Title
+        title: Text(
+          activity.title,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+
+        // Show activity description
+        // Check if null first, some activities doesn't have description
+        // For example talks have abstracts and don't feature a description
+        subtitle: (activity.desc != null) ? Text(activity.desc) : null,
+
+        // Finally show bookmark button at end
+
+        // TODO: Add a GestureDetector with stateful Icon bookmark widget
+        trailing: Icon(Icons.bookmark_border),
+
+        // On Click listener
+        onTap: () => _openTalkPage(context, activity));
   }
 
   _openTalkPage(BuildContext context, DevFestActivity talk) {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => TalkPage(talk)));
+  }
+
+  String formatTime(DateTime dateTime) {
+    final dateFormat = new DateFormat('HH:mm');
+
+    return dateFormat.format(dateTime);
   }
 }
