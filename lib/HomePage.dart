64f91a169daf0ec3113146d5eaad9429705faf0a
Paolo Rotolo <paolorotolo@ubuntu.com>
2018-07-30 15:24:32 +0200

    Add future to get user image. Implement logout on AppBar image tap.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:devfest_levante/SchedulePage.dart';
+import 'package:devfest_levante/SplashScreenPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -47,11 +49,107 @@ class HomeState extends State<HomePage> {
         theme: new ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("DevFest Levante"),
-            ),
-            bottomNavigationBar: navBar,
-            body: currentPage));
+
+        // Before showing the Scaffold, we need the profile image for the appbar
+        // So we build a Future with Firebase user request as future parameter
+        home: FutureBuilder<FirebaseUser>(
+          future: FirebaseAuth.instance.currentUser(),
+
+          // The lambda in the builder will be run each time we have updated data from Firebase
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            // Check if we're done downloading the user info
+            if (snapshot.connectionState == ConnectionState.done) {
+              // Now we can save user data in a variable
+              FirebaseUser user = snapshot.data;
+
+              return Scaffold(
+                  appBar: AppBar(
+                    // Take user data
+                    leading: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Sign out?"),
+                                  content: Text(
+                                      "Tutte le sesioni salvate e le preferenze rimarranno comunque sincronizzate con il tuo account."),
+                                  actions: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: FlatButton(
+                                        child: Text("Non ora"),
+                                        textColor: Colors.blueAccent,
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: RaisedButton(
+                                        child: Text("Sign out"),
+                                        color: Colors.blueAccent,
+                                        textColor: Colors.white,
+                                        onPressed: () {
+                                          FirebaseAuth.instance.signOut();
+                                          Navigator.pushReplacement(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder:
+                                                      (BuildContext context) =>
+                                                          SplashScreenPage()));
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                );
+                              });
+                        },
+                        child: Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: new NetworkImage(user.photoUrl)))),
+                      ),
+                    ),
+                    title: Text("DevFest Levante"),
+                  ),
+                  bottomNavigationBar: navBar,
+                  body: currentPage);
+            } else {
+              // Firebase has not returned data yet. Show loading screen
+              // TODO: Maybe replace this with a loading dialog
+              return new Text('Loading...');
+            }
+          },
+        ));
+  }
+
+  _showLogOutDialog(context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Log out"),
+            content: Text("Sicuro di voler effettuare il log out?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              SplashScreenPage()));
+                },
+              )
+            ],
+          );
+        });
   }
 }
