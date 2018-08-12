ab42cb03b3d85bd3efac0b6df6e5dff9258b05c4
Paolo Rotolo <paolorotolo@ubuntu.com>
2018-08-12 17:33:09 +0200

    Fix issue with cached speakers

--- lib/DevFestActivity.dart
+++ lib/DevFestActivity.dart
@@ -1,12 +1,12 @@
 class DevFestActivity {
-  final String id;
-  final String title;
-  final String desc;
-  final String day;
-  final DateTime start;
-  final DateTime end;
-  final String type;
-  final String speakers;
+  String id;
+  String title;
+  String desc;
+  String day;
+  DateTime start;
+  DateTime end;
+  String type;
+  String speakers = "-1";
 
   DevFestActivity.generic(this.id, this.type, this.title, this.desc, this.day,
       this.start, this.end);
